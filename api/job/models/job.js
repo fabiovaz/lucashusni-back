8a32747f765f40e5a1d435794af9e296def0f381
Fabio Vaz <fabiovaz@gmail.com>
2021-08-01 19:50:31 -0300

    feat: slugify

--- api/job/models/job.js
+++ api/job/models/job.js
@@ -5,4 +5,20 @@
  * to customize this model
  */
 
-module.exports = {};
+const slugify = require('slugify');
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.title) {
+        data.slug = slugify(data.title, {lower: true});
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.title) {
+        data.slug = slugify(data.title, {lower: true});
+      }
+    },
+  },
+};
+ 
