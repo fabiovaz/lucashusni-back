92bb651014eeb9681ff70e50e0366b325b919b1d
Fabio Vaz <fabiovaz@gmail.com>
2021-08-02 12:35:43 -0300

    fix: remove slugify

--- api/job/models/job.js
+++ api/job/models/job.js
@@ -5,20 +5,5 @@
  * to customize this model
  */
 
-const slugify = require('slugify');
-
-module.exports = {
-  lifecycles: {
-    async beforeCreate(data) {
-      if (data.title) {
-        data.slug = slugify(data.title, {lower: true});
-      }
-    },
-    async beforeUpdate(params, data) {
-      if (data.title) {
-        data.slug = slugify(data.title, {lower: true});
-      }
-    },
-  },
-};
+module.exports = {};
  
