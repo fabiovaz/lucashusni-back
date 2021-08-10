e7aeec5f576fa630ad42f99e2ca77e8c61023634
Fabio Vaz <fabiovaz@gmail.com>
2021-08-10 15:36:47 -0300

    fix: upload sizes

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,18 @@
 var credFile = require("../config/firebase.json");
 
 module.exports = () => ({
-    upload: {
-        provider: 'to-firebase',
-        providerOptions: {
-            serviceAccount: credFile ,
-            
-            bucket: "lucas-husni.appspot.com",
-            debugLogs: true
-        },
+  upload: {
+    provider: 'to-firebase',
+    providerOptions: {
+      serviceAccount: credFile ,
+    
+      bucket: "lucas-husni.appspot.com",
+      debugLogs: true
     },
-}); 
+    breakpoints: {
+      large: 1920,
+      medium: 750,
+      small: 500,
+    }
+  }
+});
